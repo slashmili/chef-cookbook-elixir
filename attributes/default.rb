01a2f593dc46414b47f0313db6423d0f362eb9ce
Milad Rastian <eslashmili@gmail.com>
2016-06-04 17:18:12 +0800

    Add recipe to download precompiled elixir from repo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
 default['elixir']['package']['version'] = nil
+
 default['elixir']['esl']['version'] = nil
+
+default['elixir']['precompiled']['version'] = '1.2.5'
+default['elixir']['precompiled']['install_path']   = "/usr/local/lib/elixir"
+default['elixir']['precompiled']['_versions_path'] = "/opt/elixir"
